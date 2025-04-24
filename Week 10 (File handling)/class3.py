43774387f5905e322350cbace3c6de94a00a4054
adeel <adeelwaraich27@gmail.com>
2025-04-24 11:18:47 +0500

    class 4

--- Week 10 (File handling)/class3.py
+++ Week 10 (File handling)/class3.py
@@ -1,23 +1,22 @@
-import csv
-# with open("sample.csv", "r") as file:
-#     variable = csv.DictReader(file)
-#     for show in variable:
-        
-#     # variable = file.read()
-#         print(show)
+# import csv
+# # with open("sample.csv", "r") as file:
+# #     variable = csv.DictReader(file)
+# #     for show in variable:
 
-with open("sample1.csv","w", newline="") as file:
-    writer = csv.writer(file)
+# #     # variable = file.read()
+# #         print(show)
+
+# with open("sample1.csv","w", newline="") as file:
+#     writer = csv.writer(file)
+
+#     data = [
+#         ["name", "roll no", "country"],
+#         ["name", "roll no", "pakistan"],
+#         ["name", "roll no", "india"]
+#     ]
     
-    data = [
-        ["name", "roll no", "country"],
-        ["name", "roll no", "pakistan"],
-        ["name", "roll no", "india"]
-    ]
+#     writer.writerows(data)
     
-    writer.writerows(data)
-    
-    # writer.writerow(["name", "age", "country"])
-    # for show in writer:
-    #     print(show)
-    
+#     # writer.writerow(["name", "age", "country"])
+#     # for show in writer:
+#     #     print(show)
---
+++ Week 10 (File handling)/class4.py
@@ -0,0 +1,57 @@
+# # # Array of arrays
+# # array_of_arrays = [
+# #     [1, 2, 3],
+# #     [4, 5, 6],
+# #     [7, 8, 9]
+# # ]
+
+# # # Array of dictionaries
+# # array_of_dicts = [
+# #     {"name": "Alice", "age": 25},
+# #     {"name": "Bob", "age": 30},
+# #     {"name": "Charlie", "age": 35}
+# # ]
+
+# # print(array_of_dicts[2]["age"])
+# # # Array of tuples
+# # array_of_tuples = [
+# #     (1, "apple"),
+# #     (2, "banana"),
+# #     (3, "cherry"),
+# # ]
+
+# # Dictionary with arrays as values
+# dict_with_arrays = {
+#     "fruits": ["apple", "banana", "cherry"],
+#     "vegetables": ["carrot", "broccoli", "spinach"]
+# }
+# print(dict_with_arrays["vegetables"][2])
+# # Dictionary with tuples as values
+# # dict_with_tuples = {
+# #     "coordinates1": (10, 20),
+# #     "coordinates2": (30, 40),
+# #     "coordinates3": (50, 60)
+# # }
+
+# # # Dictionary with dictionaries as values
+# # dict_with_dicts = {
+# #     "person1": {"name": "Alice", "age": 25},
+# #     "person2": {"name": "Bob", "age": 30},
+# #     "person3": {"name": "Charlie", "age": 35}
+# # }
+
+# # # Print all structures
+# # print("Array of arrays:", array_of_arrays)
+# # print("Array of dictionaries:", array_of_dicts)
+# # print("Array of tuples:", array_of_tuples)
+# # print("Dictionary with arrays as values:", dict_with_arrays)
+# # print("Dictionary with tuples as values:", dict_with_tuples)
+# # print("Dictionary with dictionaries as values:", dict_with_dicts)
+
+
+
+
+
+
+
+
