40e25acbb2f4e0b0b3133fd1c17b4fd12807c4e9
adeel <adeelwaraich27@gmail.com>
2025-04-28 11:25:59 +0500

    second revision class

---
+++ revision week/class1.py
@@ -0,0 +1,49 @@
+
+
+# This is a statment to print the hello world
+
+# variable = """
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# """
+
+# print(variable)
+# variable = """
+# print("hello world") 
+# print("hello world") 
+# print("hello world") 
+# print("hello world") 
+# """
+
+# variable = "     hello      world    this    our    class     "
+
+# replace = variable.replace("class", "class room")
+
+# print(replace)
+# print(variable.strip())
+# join = "  ".join(variable.split())
+# print(join)
+# print(replace)
+
+
+# var = """
+# # print something
+# "this is a variable"
+# """
+
+# print(var)
+
+
+# data types in python 
+
+
+
+# string = "this is a string"
+# integer = 22
+
+# floated = 3.13
+
+# bolean = None   # OR false
+
+
---
+++ revision week/class2.py
@@ -0,0 +1,33 @@
+# grade = float(input("write your marks here: "))
+
+# if grade >= 80:
+#     print("Your grade is A")
+    
+# elif grade >= 60:
+#     print("Your grade is B")
+    
+
+# elif grade >= 40:
+#     print("Your grade is C")
+    
+# else:
+#     print("you are failed")
+
+# names = ["ali", "ahmad", "abdullah", 22]
+
+
+# # poped_element = names.pop(1)
+# poped_element = names.remove("ali")
+# # print(dir(names))
+# print(poped_element)
+
+# for show in names:
+#     print(show)
+
+
+# names = ["ali", "ahmad", "abdullah"]
+# ages = [ 22, 30, 25]
+# marks = [ 55, 80, 90]
+
+# for show in range(len(names)):
+#     print(f"the age of {names[show]} is {ages[show]}")
